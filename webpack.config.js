3c9748b861d9be0dd589b894cef09868d08c7a99
danielVg <danielveragi@gmail.com>
2018-09-02 09:44:25 +0200

    updated packages & babelrc
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.jsx?$/,
+        test: /.js?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
